4e27aca7039bef1f8e51f1315169f592ad63e330
valance <67081858+valance1@users.noreply.github.com>
2023-05-14 12:16:03 +0000

    FunctionalForm

--- js/navbar-footer.js
+++ js/navbar-footer.js
@@ -42,6 +42,158 @@ function createHeaderAndFooter() {
 
 	var footer = `
 	`;
-	document.body.innerHTML = header + document.body.innerHTML + footer;
+
+  var modals =`
+  <!-- LOGIN Modal -->
+  <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        
+        <div class="modal-header">
+          
+          <h5 class="modal-title" id="ModalLabel">Login</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          
+        </div>
+        
+        <div class="modal-body">
+          <!-- CAMPO EMAIL -->
+            <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="exemplo@emailcom">
+              <label for="InputEmail1" class="form-label">Endereço de e-mail</label>
+            </div>
+            <!-- CAMPO  SENHA -->
+            <div class="form-floating mb-3">
+              <input type="password" class="form-control" id="InputPassword1" placeholder="******">
+              <label for="InputPassword1" class="form-label">Senha</label>
+              <!-- TEM QUE BOTAR UM BOTÃO DE "ESQUECI MINHA  SENHA" -->
+            </div>
+        </div>
+        
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+          <button type="button" class="btn btn-primary">Confirmar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!--CADASTRO Modal -->
+  <div class="modal fade" id="cadastroModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <nav>
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+
+            <button class="nav-link active" id="nav-pessoa-tab" data-bs-toggle="tab" data-bs-target="#nav-pessoa" type="button" role="tab" aria-controls="nav-pessoa" aria-selected="true">Pessoa</button>
+
+            <button class="nav-link" id="nav-empresa-tab" data-bs-toggle="tab" data-bs-target="#nav-empresa" type="button" role="tab" aria-controls="nav-empresa" aria-selected="false">Empresa</button>
+
+          </div>
+        </nav>
+          <div class="tab-content" id="nav-tabContent">
+            <!-- MODAL DE CLIENTE -->
+            <div class="tab-pane fade show active" id="nav-pessoa" role="tabpanel" aria-labelledby="nav-pessoa-tab">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cadastro de Cliente</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                
+                <div class="form-floating mb-3">
+                  <input type="text" class="form-control" id="InputNome1" aria-describedby="nomeHelp" placeholder="Nome Completo">
+                  <label for="InputNome1" class="form-label">Nome Completo</label>
+                </div>
+                
+                <div class="form-floating mb-3">
+                  <input type="number" class="form-control" id="InputCPF1" aria-describedby="CPFHelp" placeholder="CPF">
+                  <label for="InputCPF1" class="form-label">CPF</label>
+                </div>
+                
+                
+                <div class="form-floating mb-3">
+                  <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="exemplo@emailcom">
+                  <label for="InputEmail1" class="form-label">Endereço de e-mail</label>
+                </div>
+                
+                <!-- CAMPO  SENHA -->
+                <div class="form-floating mb-3">
+                  <input type="password" class="form-control" id="InputPassword1" placeholder="******">
+                  <label for="InputPassword1" class="form-label">Senha</label>
+                </div>
+                
+                <!-- CONFIRMAR SENHA -->
+                <div class="form-floating mb-3">
+                  <input type="password" class="form-control" id="InputPassword2" placeholder="******">
+                  <label for="InputPassword2" class="form-label">Confirmar Senha</label>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Confirmar</button>
+              </div>
+            </div>
+
+            <!-- MODAL DA EMPRESA -->
+            <div class="tab-pane fade" id="nav-empresa" role="tabpanel" aria-labelledby="nav-empresa-tab">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cadastro de Empresa</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                
+                <div class="form-floating mb-3">
+                  <input type="text" class="form-control" id="InputNome2" aria-describedby="nomeHelp" placeholder="Nome Completo">
+                  <label for="InputNome2" class="form-label">Nome Completo</label>
+                </div>
+                
+                <div class="form-floating mb-3">
+                  <input type="number" class="form-control" id="InputCNPJ1" aria-describedby="CNPJHelp" placeholder="CNPJ">
+                  <label for="InputCNPJ1" class="form-label">CNPJ</label>
+                </div>
+                
+                <div class="row g-2">
+                  <div class="col-md">
+                    <div class="form-floating mb-3">
+                      <input type="text" class="form-control" id="InputNumAgencia" aria-describedby="numAgenciaHelp" placeholder="Agência">
+                      <label for="numAgencia" class="form-label">Agência</label>
+                    </div>    
+                  </div>
+                  <div class="col-md">
+                    <div class="form-floating mb-3">
+                      <input type="text" class="form-control" id="InputNumConta" aria-describedby="numContaHelp" placeholder="Número da Conta">
+                      <label for="inputNumConta" class="form-label">Número da Conta</label>
+                    </div>    
+                  </div>
+                </div>
+
+                <div class="form-floating mb-3">
+                  <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="exemplo@emailcom">
+                  <label for="InputEmail1" class="form-label">Endereço de e-mail</label>
+                </div>
+                
+                <!-- CAMPO  SENHA -->
+                <div class="form-floating mb-3">
+                  <input type="password" class="form-control" id="InputPassword1" placeholder="******">
+                  <label for="InputPassword1" class="form-label">Senha</label>
+                </div>
+                
+                <!-- CONFIRMAR SENHA -->
+                <div class="form-floating mb-3">
+                  <input type="password" class="form-control" id="InputPassword2" placeholder="******">
+                  <label for="InputPassword2" class="form-label">Confirmar Senha</label>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Confirmar</button>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+	`;
+	document.body.innerHTML = header + modals + document.body.innerHTML + footer;
 }
 window.addEventListener("load", createHeaderAndFooter);
