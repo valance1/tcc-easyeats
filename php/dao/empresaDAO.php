0912bfc61593e3051c25d3ebb0f22211c18f8481
valance1 <gabrielpinotti1@gmail.com>
2023-05-16 07:46:35 -0300

    DAO Empresa

--- php/dao/empresaDAO.php
+++ php/dao/empresaDAO.php
@@ -1,24 +1,28 @@
 <?php
-function inserirPessoa($conexao, $nome, $cpf, $email, $senha )
+function inserirEmpresa($conexao, $nome, $CNPJ, $email, $senha, $agencia, $conta )
 {
         // LIMPANDO SQL INJECTION
         $email = $conexao->real_escape_string($_POST['email']);
         $senha = $conexao->real_escape_string($_POST['senha']);
+        $CNPJ = $conexao->real_escape_string($_POST['CNPJ']);
+        $nome = $conexao->real_escape_string($_POST['nome']);
+        $agencia = $conexao->real_escape_string($_POST['agencia']);
+        $conta = $conexao->real_escape_string($_POST['conta']);
         
-        $sql = "INSERT INTO pessoa (nome, cpf, email, senha) VALUES ( '$nome', '$cpf', '$email', '$senha' ) ";
+        $sql = "INSERT INTO empresa (nome, CNPJ, email, senha, agencia, conta) VALUES ( '$nome', '$CNPJ', '$email', '$senha', '$agencia', '$conta' ) ";
         mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
-function alterarPessoa(){
+function alterarEmpresa(){
 
 }
-function excluirPessoa(){
+function excluirEmpresa(){
 
 
 }
 function pesquisarNome(){
 
 }
-function pesquisarCPF(){
+function pesquisarCNPJ(){
 
 }
 ?>
