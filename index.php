124f17a0cede44a92724b8eb86728fc6b99e642e
valance1 <gabrielpinotti1@gmail.com>
2023-05-23 07:39:05 -0300

    s

--- index.php
+++ index.php
@@ -10,11 +10,12 @@ session_start();
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
+  <link href="css/main.css" type="text/css" rel="stylesheet">
+
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
 		crossorigin="anonymous"></script>
-	<link href="css/main.css" type="text/css" rel="stylesheet">
 </head>
 
 <body></body>
