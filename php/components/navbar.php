33afce89fe92dc8f3492a94e6dda68aa42fb2daa
valance1 <gabrielpinotti1@gmail.com>
2023-06-02 08:11:52 -0300

    FIX

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -4,7 +4,7 @@ error_reporting(0);
 echo '
 <nav class="navbar navbar-expand-lg bg-body-tertiary" style="padding-right: 150px; padding-left: 150px">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">EasyEats</a>
+    <a class="navbar-brand" href="index.php">EasyEats</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
