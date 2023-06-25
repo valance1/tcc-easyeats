73f675d154a30ad34eef0664d6e743e8eae26265
Gabriel Pinoti <gabrielpinotti1@gmail.com>
2023-06-24 23:17:44 -0300

    Empresa DAO

---
+++ php/controlador/editEmpresa.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "FuncoesUteis.php";
+require_once "../dao/conexaoBD.php";
+require_once "../dao/empresaDAO.php";
+
+
+?>
--- php/dao/empresaDAO.php
+++ php/dao/empresaDAO.php
@@ -1,12 +1,22 @@
 <?php
-function inserirEmpresa($conexao, $nome, $senha, $email, $CNPJ, $agencia, $conta ){
-  $sql = "INSERT INTO empresa (nome, CNPJ, email, senha, agencia, conta) VALUES ( '$nome', '$CNPJ', '$email', '$senha', '$agencia', '$conta')";
+function inserirEmpresa($conexao, $nome, $senha, $email, $cnpj, $agencia, $conta ){
+  $sql = "INSERT INTO empresa (nome, CNPJ, email, senha, agencia, conta) VALUES ( '$nome', '$cnpj', '$email', '$senha', '$agencia', '$conta')";
   mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
-function alterarEmpresa($nome){
+function alterarFotoEmpresa($conexao, $path, $cnpj){
+  $sql = "UPDATE empresa SET perfil='$path' WHERE cpf='$cnpj'";
+  mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 
 }
+function alterarAgenciaEmpresa($conexao, $agencia, $cnpj){
 
+  $sql = "UPDATE empresa SET agencia='$agencia' WHERE cpf='$cnpj'";
+  mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+}
+function alterarContaEmpresa($conexao, $conta, $cnpj){
+  $sql = "UPDATE empresa SET conta='$conta' WHERE cpf='$cnpj'";
+  mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+}
 function pesquisarNome($nome){
 
 }
