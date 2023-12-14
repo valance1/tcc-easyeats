4f58f26608448213a29c1dbebe17a0994b039648
Gabriel Pinoti <gabrielpinotti1@gmail.com>
2023-12-14 17:26:56 -0300

    Comment

--- js/searchBarHist.js
+++ js/searchBarHist.js
@@ -36,8 +36,7 @@ window.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Outro Botao
-
+    // Botão responsável por chamar o arquivo php que irá lidar com a operação de abates.
     searchButton2.addEventListener('click', function(){
         var dados = new FormData();
         dados.append('field', "abate");
@@ -63,17 +62,22 @@ window.addEventListener('DOMContentLoaded', function () {
             },
 
             complete: function (response) {
+                
+                // Exibir a resposta proveniente do arquivo PHP para o container da tabela
                 codHtml = response.responseText;
                 containerAlvo = document.getElementById('transacaoAbateTableCont');
                 containerAlvo.innerHTML = codHtml;
             },
         });
     });
+    
+    // Simplesmente para fazer com que o usuário possa apertar enter e enviar o texto para o arquivo php, sem precisar apertar na lupinha
+    // É um requisito que o campo de input seja o elemento ativo.
     document.addEventListener("keyup", function(event) {
-        if (event.code === 'Enter' && searchButton1 == document.activeElement) {
+        if (event.code === 'Enter' && document.getElementById("search-input1") == document.activeElement) {
             searchButton1.click();
         }
-        if(event.code === 'Enter' && searchButton2 == document.activeElement){
+        if(event.code === 'Enter' && document.getElementById("search-input2") == document.activeElement){
             searchButton2.click()
         }
     });
